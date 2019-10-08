06e7200189757617d69770b1c74d48a08cd14348
leografx <leografx@gmail.com>
2019-10-08 16:30:23 -0400

    style improvement

--- projects/time-planner/src/lib/time-planner.component.ts
+++ projects/time-planner/src/lib/time-planner.component.ts
@@ -16,18 +16,22 @@ export class TimePlannerComponent implements OnInit {
   selectBtn;
   selectedColor;
   selectedHours = [];
+  text = false;
+  date: string;
+  months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   @Output() hoursSelected = new EventEmitter<any>();
 
   constructor() { }
 
   ngOnInit() {
-
+    const d = new Date();
+    this.date = this.months[d.getMonth()] + ` ${d.getDate()}, ${d.getFullYear()}`;
     this.selectedColor = '#85C1C9';
   }
 
   changeColor(e) {
     if (this.select) {
-      console.log(this.selectedHours);
+      // console.log(this.selectedHours);
       this.hoursSelected.emit(this.selectedHours);
       this.selectedHours.map(data => {
         data.nativeElement.style.background = this.selectedColor;
@@ -80,32 +84,45 @@ export class TimePlannerComponent implements OnInit {
     }
   }
 
-  // eraseToggle(e) {
-  //   const item = new ElementRef(e.target);
-  //   item.nativeElement.style.background = 'transparent';
-  // }
-
   toggleSelect(e) {
+    this.changeState();
     this.selectBtn = e;
     this.selectedHours = [];
     this.select = !this.select;
-    this.erase = false;
+    // this.erase = false;
   }
 
   toggleErase(e) {
+    this.changeState();
     this.eraseBtn = e;
     this.eraseHours = [];
     this.erase = !this.erase;
-    this.select = false;
+    // this.select = false;
   }
 
-  dblClickAddLabel(e) {
-    // tslint:disable-next-line:max-line-length
-    e.target.innerHTML = `<input type="text" style="background:transparent; width:190px; height:28px; border: 0px; margin:0px; padding:0px 5px;"  value=""/>`;
+  toggleText(e) {
+    this.changeState();
+    this.text = !this.text;
+  }
+
+  addInput(e) {
+    if (this.text) {
+      const id = e.target.id;
+      console.log(id);
+      // tslint:disable-next-line:max-line-length
+      e.target.innerHTML = `<input type="text" id="${id + '-input'}" style="background:transparent; width:190px; height:28px; border: 0px; margin:0px; padding:0px 5px;"  value=""/>`;
+      e.target.children[0].focus();
+    }
   }
 
   colorSelected(e) {
     this.selectedColor = e.target.value;
+  }
+
+  changeState() {
+    this.text = false;
+    this.select = false;
+    this.erase = false;
   }
 
 }
